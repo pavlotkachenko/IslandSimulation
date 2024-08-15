d2a426ab5df55a4e844c8a59c0fa2d1c58cdeb81
Pr1nkos <prinkos1@gmail.com>
2024-08-15 17:43:06 +0300

    new: first implementation of services #2

--- island/src/main/java/org/island/services/AnimalFeedingService.java
+++ island/src/main/java/org/island/services/AnimalFeedingService.java
@@ -1,12 +1,40 @@
 package org.island.services;
 
 import org.island.entity.OrganismDTO;
-import org.island.model.Island;
 import org.island.model.Location;
 
-public class AnimalFeedingService implements FeedingService {
-    @Override
-    public void eat(OrganismDTO organismDTO, Location location) {
+import java.util.Map;
+import java.util.Set;
 
+public class AnimalFeedingService implements FeedingService {
+
+    @Override
+    public void eat(OrganismDTO organism, Location location) {
+        location.getLock().lock();
+
+        try {
+            Map<String, Integer> ration = organism.getRation();
+
+            for (Map.Entry<String, Integer> entry : ration.entrySet()) {
+                String preyType = entry.getKey();
+                int probability = entry.getValue();
+
+                Set<OrganismDTO> preySet = location.getResidents().get(preyType);
+
+                if (preySet != null && !preySet.isEmpty()) {
+                    for (OrganismDTO prey : preySet) {
+                        if (Math.random() * 100 < probability) {
+                            organism.setCurrentWeight(Math.min(organism.getMaxWeight(),
+                                    organism.getCurrentWeight() + prey.getCurrentWeight()));
+                            prey.setAlive(false);
+                            location.getResidents().get(preyType).remove(prey);
+                            break;
+                        }
+                    }
+                }
+            }
+        } finally {
+            location.getLock().unlock();
+        }
     }
 }
